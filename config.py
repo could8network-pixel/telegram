67e82e3928dedf0c7ea8c856611e8ba6efbedc91
abin <could8network@gmail.com>
2026-06-29 13:55:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("8656583035:AAEuvWLHdy_LbMw5VgP4L7dNvbYqSU7jNZA")
-USER_ID = os.getenv(" 476093481")
+USER_ID = os.getenv("10")
 API_TOKEN = os.getenv("5be6a2daa545f53d80d484cc29f4b979")
 GEO_CODE = os.getenv("9.683,76.433")
 PINCODE = os.getenv("686607")
